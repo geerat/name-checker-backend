2d207f33f5e34c972bfca1df3b9a34c6ced72006
Chester Jerrat <chester@tradewindow.io>
2020-02-05 09:49:47 +1300

    Created spring maven project

---
+++ src/main/java/com/chesterjerrat/namechecker/NamecheckerApplication.java
@@ -0,0 +1,13 @@
+package com.chesterjerrat.namechecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NamecheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NamecheckerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chesterjerrat/namechecker/NamecheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chesterjerrat.namechecker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamecheckerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
